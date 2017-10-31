6bd98d67041dc24970d7766886574122d1667246
jrks1998 <jonas.schneider@institutoivoti.com.br>
2017-10-31 19:56:11 -0200

    Alterações no codigo

--- Validacao.cs
+++ Validacao.cs
@@ -4,18 +4,20 @@ namespace console_dotnet
 {    
     class Validacao
     {
-        public void numero(int a, int b) {
+        public Boolean numero(int a, int b) {
             Random r = new Random();
             int n = r.Next(a, b);
             int s = r.Next(a, b);
-            int resul = n + s;
+            int soma = n + s;
             System.Console.WriteLine(n + " + " + s + " = ");
             int resposta = Convert.ToInt32(Console.ReadLine());
 
-            if (resposta == resul) {
+            if (resposta == soma) {
                 System.Console.WriteLine("Acesso liberado!");
+                return true;
             } else {
                 System.Console.WriteLine("Acesso bloqueado!");
+                return false;
             }
         }
     }
